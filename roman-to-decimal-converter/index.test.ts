91d94f7d492f0d3a3fd89c2d7c139e3900c48561
searchingforapuppy <ebisulove2@gmail.com>
2021-07-23 14:56:00 +0900

    Implement roman to decimal converter function

--- roman-to-decimal-converter/index.test.ts
+++ roman-to-decimal-converter/index.test.ts
@@ -5,7 +5,12 @@ describe("Roman to decimal converter test", () => {
     expect(romanToDecimal("I")).toBe(1);
     expect(romanToDecimal("V")).toBe(5);
     expect(romanToDecimal("X")).toBe(10);
-    expect(romanToDecimal("IV")).toBe(4);
-    expect(romanToDecimal("XX")).toBe(20);
+    expect(romanToDecimal("L")).toBe(50);
+    expect(romanToDecimal("C")).toBe(100);
+    expect(romanToDecimal("M")).toBe(1000);
+    expect(romanToDecimal("CLX")).toBe(160);
+    expect(romanToDecimal("CCVII")).toBe(207);
+    expect(romanToDecimal("MIX")).toBe(1009);
+    expect(romanToDecimal("MDCCLXXVI")).toBe(1776);
   });
 });
--- roman-to-decimal-converter/index.ts
+++ roman-to-decimal-converter/index.ts
@@ -3,6 +3,7 @@ export const romanToDecimal = (roman: string): number => {
 
   let current = 0;
   while (current < roman.length) {
+    // units
     if (roman[current] === "I") {
       if (roman[current + 1] === "V") {
         tokens.push(4);
@@ -19,8 +20,48 @@ export const romanToDecimal = (roman: string): number => {
       tokens.push(5);
       current += 1;
     }
+
+    // tens
     if (roman[current] === "X") {
-      tokens.push(10);
+      if (roman[current + 1] === "L") {
+        tokens.push(40);
+        current += 2;
+      } else if (roman[current + 1] === "C") {
+        tokens.push(90);
+        current += 2;
+      } else {
+        tokens.push(10);
+        current += 1;
+      }
+    }
+
+    if (roman[current] === "L") {
+      tokens.push(50);
+      current += 1;
+    }
+
+    // hundreds
+    if (roman[current] === "C") {
+      if (roman[current + 1] === "D") {
+        tokens.push(400);
+        current += 2;
+      } else if (roman[current + 1] === "M") {
+        tokens.push(900);
+        current += 2;
+      } else {
+        tokens.push(100);
+        current += 1;
+      }
+    }
+
+    if (roman[current] === "D") {
+      tokens.push(500);
+      current += 1;
+    }
+
+    // thousands
+    if (roman[current] === "M") {
+      tokens.push(1000);
       current += 1;
     }
   }
