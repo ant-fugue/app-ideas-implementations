fa962a42a5fc26bff32a281539204871957c53c2
searchingforapuppy <ebisulove2@gmail.com>
2021-07-29 16:47:25 +0900

    add error handling logics for calculator cli

--- calculator-cli/src/arith.test.ts
+++ calculator-cli/src/arith.test.ts
@@ -2,12 +2,15 @@ import { add, odd, even } from "./arith";
 
 describe("arith module test", () => {
   test("add() returns the sum of given numbers", () => {
-    expect(add([3, 5])).toBe(8);
+    expect(() => add(["3.1", "5.2"])).toThrow("arguments must be integers");
+    expect(add(["3", "5"])).toBe(8);
   });
   test("even() returns the sum of given even numbers", () => {
+    expect(() => even([2, 3])).toThrow("arguments must be even integers");
     expect(even([2, 4])).toBe(6);
   });
   test("odd() returns the sum of given odd numbers", () => {
+    expect(() => odd([2, 4])).toThrow("arguments must be odd integers");
     expect(odd([3, 5])).toBe(8);
   });
 });
--- calculator-cli/src/arith.ts
+++ calculator-cli/src/arith.ts
@@ -1,28 +1,28 @@
-export const add = (arr: any[]): number => {
-  const items = arr.map((elem) => parseInt(elem, 10));
+export const add = (arr: string[]): number => {
+  const items = arr.map((elem) => Number(elem));
   if (items.some((item) => !Number.isInteger(item))) {
-    throw Error("arguments must be even integers");
+    throw new Error("arguments must be integers");
   }
   return items.reduce((acc, cur) => acc + cur, 0);
 };
 
 export const even = (arr: any[]): number => {
-  const items: number[] = arr.map((elem) => parseInt(elem, 10));
+  const items: number[] = arr.map((elem) => Number(elem));
   if (
     items.some((item) => !Number.isInteger(item)) ||
     items.some((item) => item % 2 === 1)
   ) {
-    throw Error("arguments must be even integers");
+    throw new Error("arguments must be even integers");
   }
   return items.reduce((acc, cur) => acc + cur, 0);
 };
 export const odd = (arr: any[]): number => {
-  const items: number[] = arr.map((elem) => parseInt(elem, 10));
+  const items: number[] = arr.map((elem) => Number(elem));
   if (
     items.some((item) => !Number.isInteger(item)) ||
     items.some((item) => item % 2 === 0)
   ) {
-    throw Error("arguments must be odd integers");
+    throw new Error("arguments must be odd integers");
   }
   return items.reduce((acc, cur) => acc + cur, 0);
 };
